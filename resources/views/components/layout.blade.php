973964e0e5eda950655c0ee93ca174885f957aa5
LE-PHU-HAI <lephuhai07062001@gmail.com>
2024-01-27 16:21:44 +0900

    chap-3-完成

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,8 +8,10 @@
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
     <script src="{{ mix('/js/app.js') }}" async defer></script>
     <title>{{ $title ?? 'つぶやきアプリ' }}</title>
+    @stack('css')
 </head>
 <body class="bg-gray-50">
     {{ $slot }}
+    {{-- <aside>{{ $aside}}</aside> --}}
 </body>
 </html>
